e14c7ec5e8fe6635780f12196cd76c9f6b106ce9
Ryusen Sasa <ryusensasa@Ryusen-no-MacBook-Pro.local>
2014-02-01 20:44:11 +0900

    test

--- unknown/ViewController.m
+++ unknown/ViewController.m
@@ -8,16 +8,37 @@
 
 #import "ViewController.h"
 
+#define keyId @"id"
+#define keyTitle @"title"
+
 @interface ViewController ()
+
 
 @end
 
 @implementation ViewController
 
+
+-(void)getTimeLine {
+    NSString* url = @"http://localhost:3000/books.json";
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    NSData *json_data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSError *error=nil;
+    
+    NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:json_data options:NSJSONReadingAllowFragments error:&error];
+    NSLog(@"%@",[jsonArray objectAtIndex:0]);
+
+
+}
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self getTimeLine];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +46,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
